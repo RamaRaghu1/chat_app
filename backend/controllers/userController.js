9103993038a10623aaef9657da3b8889bef500f9
Rama Raghu <ramanadar003@gmail.com>
2025-02-07 16:37:25 +0530

    get all users api completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,4 +63,14 @@ const logout = (req, res) => {
 		res.status(500).json({ error: error.message });
 	}
 };
-export { signup, login ,logout};
+
+const getUsers=async(req,res)=>{
+    try{
+const loggedInUserId=req.user._id;
+const allUsers=await User.find({_id: {$ne:loggedInUserId}});
+res.status(200).json(allUsers)
+    }catch(error){
+        res.status(500).json({error:error.message})
+    }
+}
+export { signup, login ,logout, getUsers};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/userController.js";
-
+import { login, logout, signup, getUsers } from "../controllers/userController.js";
+ import protectdRoute from "../middleware/protectedRoute.js";
 
 const userRoutes=express.Router();
 userRoutes.post('/signup', signup);
 userRoutes.post('/login', login);
 userRoutes.post('/logout', logout);
+userRoutes.get('/users',protectdRoute,getUsers);
 
 export default userRoutes
